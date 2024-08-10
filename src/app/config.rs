9ffd3c930b5bbf2f0e333d4dd160f6dfb5995b7d
David Tadokoro <davidbtadokoro@usp.br>
2024-08-10 13:47:13 -0300

    src: app: config: Refactor cache and data dir names
    To make `patch-hub` less coupled with `kw` remove it from the prefix of
    environment variables and cache and data directory paths.
    
    Signed-off-by: David Tadokoro <davidbtadokoro@usp.br>

--- src/app/config.rs
+++ src/app/config.rs
@@ -24,24 +24,24 @@ impl Config {
             Err(_) => 30,
         };
 
-        patchsets_cache_dir = match env::var("KW_CACHE_DIR") {
-            Ok(value) => format!("{value}/patch_hub/patchsets"),
-            Err(_) => format!("{}/.cache/kw/patch_hub/patchsets", env::var("HOME").unwrap()),
+        patchsets_cache_dir = match env::var("PATCH_HUB_CACHE_DIR") {
+            Ok(value) => format!("{value}/patchsets"),
+            Err(_) => format!("{}/.cache/patch_hub/patchsets", env::var("HOME").unwrap()),
         };
 
-        bookmarked_patchsets_path = match env::var("KW_DATA_DIR") {
-            Ok(value) => format!("{value}/patch_hub/bookmarked_patchsets.json"),
-            Err(_) => format!("{}/.local/share/kw/patch_hub/bookmarked_patchsets.json", env::var("HOME").unwrap()),
+        bookmarked_patchsets_path = match env::var("PATCH_HUB_DATA_DIR") {
+            Ok(value) => format!("{value}/bookmarked_patchsets.json"),
+            Err(_) => format!("{}/.local/share/patch_hub/bookmarked_patchsets.json", env::var("HOME").unwrap()),
         };
 
-        mailing_lists_path = match env::var("KW_DATA_DIR") {
-            Ok(value) => format!("{value}/patch_hub/mailing_lists.json"),
-            Err(_) => format!("{}/.local/share/kw/patch_hub/mailing_lists.json", env::var("HOME").unwrap()),
+        mailing_lists_path = match env::var("PATCH_HUB_DATA_DIR") {
+            Ok(value) => format!("{value}/mailing_lists.json"),
+            Err(_) => format!("{}/.local/share/patch_hub/mailing_lists.json", env::var("HOME").unwrap()),
         };
 
-        reviewed_patchsets_path = match env::var("KW_DATA_DIR") {
-            Ok(value) => format!("{value}/patch_hub/reviewed_patchsets.json"),
-            Err(_) => format!("{}/.local/share/kw/patch_hub/reviewed_patchsets.json", env::var("HOME").unwrap()),
+        reviewed_patchsets_path = match env::var("PATCH_HUB_DATA_DIR") {
+            Ok(value) => format!("{value}/reviewed_patchsets.json"),
+            Err(_) => format!("{}/.local/share/patch_hub/reviewed_patchsets.json", env::var("HOME").unwrap()),
         };
 
         Config {
